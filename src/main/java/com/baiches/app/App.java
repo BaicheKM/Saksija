c826f1f3efa48622c68ac10bccac0629a954b21b
Milos Mihajlovic <miloskm@gmail.com>
2017-10-05 20:36:20 +0200

    First function

--- src/main/java/com/baiches/app/App.java
+++ src/main/java/com/baiches/app/App.java
@@ -1,5 +1,10 @@
 package com.baiches.app;
 
+import de.btobastian.javacord.DiscordAPI;
+import de.btobastian.javacord.Javacord;
+import de.btobastian.javacord.entities.message.Message;
+import de.btobastian.javacord.listener.message.MessageCreateListener;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,17 @@ public class App
 {
     public static void main( String[] args )
     {
+    	DiscordAPI api = Javacord.getApi("MzY1NTYxNDQ1ODE3MTIyODE3.DLgHNA.EgsPYX_eJxKr5RHXeStj3YBY0zA", true);
+    	api.connectBlocking();
+    	api.registerListener(new MessageCreateListener() {
+			
+			public void onMessageCreate(DiscordAPI api, Message message) {
+				if (message.getContent().equalsIgnoreCase("ping")) {
+					message.reply("/tts pong");
+				}
+			}
+		});
+    	
         System.out.println( "Hello World!" );
     }
 }
